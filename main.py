0b0bfb50888d7be2563990b140e4b1d4daef6ffe
MaxLuc26 <maxlucasberman@gmail.com>
2025-12-09 19:37:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from string_utils import parse_chemical_reaction, count_atoms_in_reaction
+from equation_utils import build_equations, my_solve
 
 def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
 
